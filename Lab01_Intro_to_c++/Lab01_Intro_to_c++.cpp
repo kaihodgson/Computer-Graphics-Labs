5a155c79d0d9be2d0e860a68e6c2301e1816deb8
Kai Hodgson <146730176+kaihodgson@users.noreply.github.com>
2025-01-29 14:43:50 +0000

    Did some of the Lab 1 activities

--- Lab01_Intro_to_c++/Lab01_Intro_to_c++.cpp
+++ Lab01_Intro_to_c++/Lab01_Intro_to_c++.cpp
@@ -1,1 +1,15 @@
+#include <iostream>
 
+int main()
+// Methods
+void outputDetails();
+<return type> <class name> :: <method name>()
+{
+    void Car::outputDetails()
+    {
+        std::cout << "\nMake: " << make
+            << "\nModel: " << model
+            << "\nYear: " << year << std::endl;
+    }
+    std::cout << "\nClasses\n-------" << std::endl;
+    delorean.outputDetails();
